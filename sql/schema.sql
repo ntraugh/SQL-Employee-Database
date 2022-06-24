d34a400bfd1bf6004641abdaafb9d4e1089624cf
Nate Traugh <n.traugh@yahoo.com>
2022-06-24 15:17:45 -0400

    updated schema.sql to look like readme directions

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,12 +2,22 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
-CREATE TABLE all_employees (
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+); 
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL, 
+    department_id INT
+);
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    manager VARCHAR(50)
-); 
+    role_id INT NOT NULL,
+    manager_id INT
+);
+   
