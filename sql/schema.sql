26d51c9d6407e31fc914cc1d118d585796683c71
Nate Traugh <n.traugh@yahoo.com>
2022-06-24 10:36:39 -0400

    added table information for all_employees

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,5 +3,11 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE all_employees (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(50)
 ); 
